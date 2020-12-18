494fbacaa10001bdc66357a94e5fd28d69f992f8
Andreas Bender <andreas.bender@stat.uni-muenchen.de>
2020-12-18 05:20:07 +0000

    version 0.5.4

--- R/as-ped.R
+++ R/as-ped.R
@@ -4,6 +4,7 @@
 #' \code{pammtools} package. Two main applications must be distinguished:
 #' \enumerate{
 #'  \item Transformation of standard time-to-event data.
+#'  \item Transformation of left-truncated time-to-event data.
 #'  \item Transformation of time-to-event data with time-dependent covariates (TDC).
 #' }
 #' For the latter, the type of effect one wants to estimate is also
@@ -17,21 +18,25 @@
 #'
 #' @rdname as_ped
 #' @param data Either an object inheriting from data frame or in case of
-#' time-dependent covariates a list of data frames, where the first data frame
+#' time-dependent covariates a list of data frames (of length 2), where the first data frame
 #' contains the time-to-event information and static covariates while the second
 #' (and potentially further data frames) contain information on time-dependent
 #' covariates and the times at which they have been observed.
 #' @param formula A two sided formula with a \code{\link[survival]{Surv}} object
 #' on the left-hand-side and covariate specification on the right-hand-side (RHS).
 #' The RHS can be an extended formula, which specifies how TDCs should be transformed
-#' using specials \code{concurrent} and \code{cumulative}.
-#' @param cut Break points, used to partition the follow up into intervals.
+#' using specials \code{concurrent} and \code{cumulative}. The left hand-side can
+#' be in start-stop-notation. This, however, is only used to create left-truncated
+#' data and does not support the full functionality.
+#' @param cut Split points, used to partition the follow up into intervals.
 #' If unspecified, all unique event times will be used.
 #' @param max_time If \code{cut} is unspecified, this will be the last
 #' possible event time. All event times after \code{max_time}
 #' will be administratively censored at \code{max_time}.
 #' @param tdc_specials A character vector. Names of potential specials in
 #' \code{formula} for concurrent and or cumulative effects.
+#' @param censor_code Specifies the value of the status variable that indicates censoring.
+#' Often this will be \code{0}, which is the default.
 #' @param ... Further arguments passed to the \code{data.frame} method and
 #' eventually to \code{\link[survival]{survSplit}}
 #' @importFrom Formula Formula
@@ -53,20 +58,41 @@ as_ped.data.frame <- function(
   cut          = NULL,
   max_time     = NULL,
   tdc_specials = c("concurrent", "cumulative"),
+  censor_code  = 0L,
+  transition   = character(),
+  timescale    = c("gap", "calendar"),
+  min_events   = 1L,
   ...) {
 
   status_error(data, formula)
   assert_subset(tdc_specials, c("concurrent", "cumulative"))
 
-  dots <- list(...)
-  dots$data <- data
-  formula <- get_ped_form(formula, data = data, tdc_specials = tdc_specials)
-  dots$formula  <- formula
-  dots$cut <- cut
-  dots$max_time <- max_time
-  ped <- do.call(split_data, dots)
-  attr(ped, "time_var") <- get_lhs_vars(formula)[1]
-  attr(ped, "status_var") <- get_lhs_vars(formula)[2]
+  if (test_character(transition, min.chars = 1L, min.len = 1L)) {
+    ped <- as_ped_recurrent(data = data, formula = formula, cut = cut,
+      max_time = max_time, tdc_specials = tdc_specials, censor_code = censor_code,
+      transition = transition, timescale = timescale, min_events = min_events, ... )
+    return(ped)
+  }
+
+  event_types <- get_event_types(data, formula, censor_code)
+  if (length(event_types) > 1) {
+
+    ped <- as_ped_cr(data = data, formula = formula, cut = cut, max_time = max_time,
+      tdc_specials = tdc_specials, censor_code = censor_code, ...)
+
+  } else {
+
+    dots          <- list(...)
+    dots$data     <- data
+    dots$formula  <- get_ped_form(formula, data = data, tdc_specials = tdc_specials)
+    dots$cut      <- cut
+    dots$max_time <- max_time
+
+    ped <- do.call(split_data, dots)
+    attr(ped, "time_var") <- get_lhs_vars(dots$formula)[1]
+    attr(ped, "status_var") <- get_lhs_vars(dots$formula)[2]
+
+  }
 
   ped
 
@@ -88,8 +114,9 @@ as_ped.nested_fdf <- function(data, formula, ...) {
   cut <- union(cut, ccr_breaks[ccr_breaks <= max(cut)]) %>% sort()
 
   ped <- data %>%
-    select_if (is.atomic) %>%
-    as_ped.data.frame(
+    select_if(is.atomic) %>%
+    as.data.frame() %>%
+    as_ped(
       formula  = formula,
       id       = dots$id,
       cut      = cut,
@@ -116,8 +143,8 @@ as_ped.nested_fdf <- function(data, formula, ...) {
   }
   attr(ped, "time_var") <- get_lhs_vars(formula)[1]
   attr(ped, "func_mat_names") <- make_mat_names(
-    attr(ped, "func"),
-    attr(ped, "time_var"))
+  attr(ped, "func"),
+  attr(ped, "time_var"))
 
   ped
 
@@ -195,5 +222,149 @@ as_ped.pamm <- function(data, newdata, ...) {
   trafo_args      <- data[["trafo_args"]]
   trafo_args$data <- newdata
   do.call(split_data, trafo_args)
+
+}
+
+
+#' Competing risks trafo
+#' @inherit as_ped
+#' @importFrom rlang .env
+#'
+#' @keywords internal
+as_ped_cr <- function(
+  data,
+  formula,
+  cut          = NULL,
+  max_time     = NULL,
+  tdc_specials = c("concurrent", "cumulative"),
+  censor_code  = 0L,
+  combine      = TRUE,
+  ...) {
+
+  lhs_vars <- get_lhs_vars(formula)
+  event_types <- get_event_types(data, formula, censor_code)
+
+  cut <- map2(
+    event_types,
+    if(is.list(cut)) cut else list(cut),
+    function(.event, .cut) {
+      get_cut(data, formula = formula, cut = .cut, max_time = NULL, event = .event)
+    }
+  )
+  if(length(cut) > 1 & combine) {
+    cut <- list(reduce(cut, union))
+  }
+
+  ped <- map2(
+    event_types,
+    cut,
+    function(.event, .cut) {
+      ped_i <- data %>%
+        mutate(!!lhs_vars[2] := 1L * (.data[[lhs_vars[2]]] == .env[[".event"]])) %>%
+        as_ped(
+          formula      = formula,
+          cut          = .cut,
+          max_time     = max_time,
+          tdc_specials = tdc_specials,
+          ...)
+      ped_i$cause <- .event
+      ped_i
+    })
+
+  if (combine) {
+    ped <- do.call(rbind, ped)
+    class(ped) <- c("ped_cr_union", "ped_cr", class(ped))
+    attr(ped, "intvars") <- c(attr(ped, "intvars"), "cause")
+    attr(ped, "breaks") <- if (length(cut) ==1) unlist(cut) else cut
+  } else {
+    class(ped) <- c("ped_cr_list", "ped_cr", "ped", class(ped))
+    names(ped) <- paste0("cause = ", event_types)
+    attributes(ped)$trafo_args$id <- attributes(ped[[1]])$trafo_args$id
+    attributes(ped)$trafo_args$formula <- formula
+  }
+
+  attr(ped, "trafo_args")[["cut"]] <- if (length(cut) ==1) unlist(cut) else cut
+  attr(ped, "trafo_args")[["combine"]] <- combine
+  attr(ped, "trafo_args")[["censor_code"]] <- censor_code
+  attr(ped, "risks") <- event_types
+
+  ped
+
+}
+
+#' Exctract event types
+#'
+#' Given a formula that specifies the status variable of the outcome, this function
+#' extracts the different event types (except for censoring, specified by
+#' \code{censor_code}).
+#'
+#' @inheritParams as_ped
+#'
+#' @keywords internal
+get_event_types <- function(data, formula, censor_code) {
+
+  lhs_vars <- get_lhs_vars(formula)
+  status_values <- unique(data[[lhs_vars[length(lhs_vars)]]]) %>% sort()
+  status_values[status_values != censor_code]
+
+}
+
+
+
+#' Recurrent events trafo
+#'
+#' @examples
+#' \dontrun{
+#' data("cgd", package = "frailtyHL")
+#' cgd2 <- cgd %>%
+#'  select(id, tstart, tstop, enum, status, age) %>%
+#'  filter(enum %in% c(1:2))
+#' ped_re <- as_ped_recurrent(
+#'   formula = Surv(tstart, tstop, status) ~ age + enum,
+#'   data = cgd2,
+#'  transition = "enum")
+#' }
+#' @rdname as_ped
+#' @export
+#' @keywords internal
+as_ped_recurrent <- function(
+  data,
+  formula,
+  cut          = NULL,
+  max_time     = NULL,
+  tdc_specials = c("concurrent", "cumulative"),
+  censor_code  = 0L,
+  transition  = character(),
+  timescale    = c("gap", "calendar"),
+  min_events   = 1L,
+  ...
+) {
+
+  assert_character(transition, min.chars = 1L, min.len = 1L, any.missing = FALSE,
+    len = 1L)
+  assert_integer(min_events, lower = 1L, len = 1L)
+
+  status_error(data, formula)
+  assert_subset(tdc_specials, c("concurrent", "cumulative"))
+
+  rhs_vars <- get_rhs_vars(formula)
+  if (!(transition %in% rhs_vars)) {
+    formula <- add_to_rhs(formula, transition)
+  }
+
+  dots            <- list(...)
+  dots$data       <- data
+  dots$formula    <- get_ped_form(formula, data = data, tdc_specials = tdc_specials)
+  dots$cut        <- cut
+  dots$max_time   <- max_time
+  dots$transition <- transition
+  dots$min_events <- min_events
+  dots$timescale  <- timescale
+
+  ped <- do.call(split_data_recurrent, dots)
+  attr(ped, "time_var")   <- get_lhs_vars(dots$formula)[1]
+  attr(ped, "status_var") <- get_lhs_vars(dots$formula)[2]
+
+  return(ped)
 
 }
